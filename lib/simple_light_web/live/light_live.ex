1a9b9bcd400c4d2c1fe03172abaa32a59441c3cb
Marcel <marcel.nakamine@outlook.com>
2020-07-03 23:36:40 -0300

    Spliting the template into a dedicated file

--- lib/simple_light_web/live/light_live.ex
+++ lib/simple_light_web/live/light_live.ex
@@ -5,33 +5,6 @@ defmodule SimpleLightWeb.LightLive do
     {:ok, assign(socket, :brightness, 10)}
   end
 
-  def render(assigns) do
-    ~L"""
-    <h1>Front Porch Light</h1>
-    <div class="meter">
-      <span style="width: <%= @brightness %>%">
-        <%= @brightness %>%
-      </span>
-    </div>
-
-    <button phx-click="off">
-      Off
-    </button>
-
-    <button phx-click="down">
-      Down
-    </button>
-    
-    <button phx-click="up">
-      Up
-    </button>
-
-    <button phx-click="on">
-      On
-    </button>
-    """
-  end
-
   def handle_event("on", _, socket) do
     socket = assign(socket, :brightness, 100)
     {:noreply, socket}
